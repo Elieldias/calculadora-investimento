5fc8934cc264fd7af84a3b713724b7448bc4ef4a
Elieldias <eliel10diasreis@gmail.com>
2024-10-08 14:43:30 -0300

    ateração no button/onclick

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ function calculateInvestiment(){
     var principal= parseFloat( document.getElementById('principal').value);
     
     var interestrate= parseFloat(document.getElementById('interest').value);
-    var years=parseFloat(document.getElementById('years').value);
+    var years=parseInt(document.getElementById('years').value);
+    if(isNaN(principal)||isNaN(interestrate) isNaN(years) ){
+        document.getElementById('result').textContent="Por Favor,Insira Valores Válidos.";
+   return;
+    }
     var futurevalue=principal*Math.pow(1 +(interestrate / 100), years);
-    var resultElement=document.getElementById('result');
+
+    
+
     var resultElement=documet.getElementById('result');
 resultElement.textContent=`Valor futuro do investimento:R$ ${ futurevalue.toFixed(2)}`;
 }
